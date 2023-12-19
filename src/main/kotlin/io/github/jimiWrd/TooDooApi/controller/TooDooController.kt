c3b157e3f3a338a85a74d73d91c68482dc59095b
Jimi Ward <jimi.r.ward@gmail.com>
2023-12-19 19:27:08 +0000

    add getTooDoo functionality, add basic exception handling

--- src/main/kotlin/io/github/jimiWrd/TooDooApi/controller/TooDooController.kt
+++ src/main/kotlin/io/github/jimiWrd/TooDooApi/controller/TooDooController.kt
@@ -3,9 +3,12 @@ package io.github.jimiWrd.TooDooApi.controller
 import io.github.jimiWrd.TooDooApi.dto.TooDooRequestDto
 import io.github.jimiWrd.TooDooApi.dto.TooDooResponseDto
 import io.github.jimiWrd.TooDooApi.service.TooDooService
+import jakarta.persistence.Id
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.repository.query.Param
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -15,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("api/v1/toodoo")
 class TooDooController @Autowired constructor(private val tooDooService: TooDooService) {
 
-    @PostMapping("/create")
+    @PostMapping("/create", consumes = ["application/json"])
     fun createTooDoo(@RequestBody tooDooRequestDto: TooDooRequestDto): ResponseEntity<TooDooResponseDto> {
         val response = tooDooService.createNewTooDooTask(tooDooRequestDto)
         return ResponseEntity.ok(response)
@@ -23,7 +26,21 @@ class TooDooController @Autowired constructor(private val tooDooService: TooDooS
 
     @GetMapping("/getAll")
     fun getAllTooDoo(): ResponseEntity<List<TooDooResponseDto>> {
-        val response = tooDooService.getAllTooDooTasks()
-        return ResponseEntity.ok(response)
+        return try {
+            val response = tooDooService.getAllTooDooTasks()
+            ResponseEntity.ok(response)
+        } catch (e: Exception) {
+            ResponseEntity.badRequest().build()
+        }
+    }
+
+    @GetMapping("/get/{id}")
+    fun getTooDoo(@PathVariable("id") id: Long): ResponseEntity<TooDooResponseDto> {
+        return try {
+            val response = tooDooService.getTooDoo(id)
+            ResponseEntity.ok(response)
+        } catch (e: Exception) {
+            ResponseEntity.badRequest().build()
+        }
     }
 }
--- src/main/kotlin/io/github/jimiWrd/TooDooApi/repository/TooDooRepository.kt
+++ src/main/kotlin/io/github/jimiWrd/TooDooApi/repository/TooDooRepository.kt
@@ -3,7 +3,9 @@ package io.github.jimiWrd.TooDooApi.repository
 import io.github.jimiWrd.TooDooApi.model.TooDooTask
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import java.util.Optional
 
 @Repository
 interface TooDooRepository: JpaRepository<TooDooTask, Long> {
+    override fun findById(id: Long): Optional<TooDooTask>
 }
